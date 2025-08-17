467682627b76e88682940f129cce9fa95594b4f5
James Hatfield <james.hatfield17@icloud.com>
2025-08-17 19:14:56 +0100

    Created a method that allows user to input a number for calculation

--- mathGame.jjhh17/Program.cs
+++ mathGame.jjhh17/Program.cs
@@ -9,22 +9,39 @@ int attempts = 0;
 // Generating random numbers
 Random random = new Random();
 int number1 = random.Next(1, 50);
-int number2= random.Next(1, 50);
+int number2 = random.Next(1, 50);
 int answer;
 char operatorInput = '+';
 
+// Selecting the operator based on user input
 OperatorSelection();
+// Calculating the randomly generated answer
 AnswerCalculation(number1, number2);
+// Printing the question for user
+Console.WriteLine($"Question {attempts + 1}: {number1} {operatorInput} {number2}");
+// Taking the number input from the user
+int userNumber1 = numberInput();
+int userNumber2 = numberInput();
 
 // User selects an operator
 void OperatorSelection()
 {
     Console.WriteLine("Enter a number to select an operator");
     Console.WriteLine("1. Addition, 2. Subtraction, 3. Division, 4. Multiplication");
-    string operatorString = Console.ReadLine();
     int operatorInt;
 
-    int.TryParse(operatorString, out operatorInt);
+    while (true)
+    {
+        string operatorString = Console.ReadLine();
+        if (int.TryParse(operatorString, out operatorInt))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Please enter a valid number");
+        }
+    }
 
     switch (operatorInt)
     {
@@ -62,5 +79,24 @@ void AnswerCalculation(int num1, int num2)
         case '*':
             answer = num1 * num2;
             break;
+    }
+}
+
+int numberInput()
+{
+    Console.WriteLine("Enter a number");
+    int numberInt;
+
+    while (true)
+    {
+        string number = Console.ReadLine();
+        if (int.TryParse(number, out numberInt))
+        {
+            return numberInt;
+        }
+        else
+        {
+            Console.WriteLine("Invalid number, please try again");
+        }
     }
 }
