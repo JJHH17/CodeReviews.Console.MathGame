396b840d5739d45814bca3feecb550ea10b08a43
James Hatfield <james.hatfield17@icloud.com>
2025-08-17 11:56:57 +0100

    Added logic to successfully parse string number input to an integer

--- mathGame.jjhh17/Program.cs
+++ mathGame.jjhh17/Program.cs
@@ -1,12 +1,4 @@
-﻿/* 
-
-if number 1 and number 2 operate to the answer, increase 1 to the score and keep loop running
-
-if the answer if wrong, end the loop and print their score
-
-*/
-
-
+﻿
 PrintIntro();
 
 // Score counter, if it reaches 5 they win
@@ -17,35 +9,38 @@ Random random = new Random();
 int number1 = random.Next(0, 30);
 int number2 = random.Next(0, 30);
 
-// Loop program for user, 5 times
+Console.WriteLine("Enter number 1");
+int number1Int;
+
 while (true)
 {
-    Console.WriteLine("Enter an operator (+ Addition, - Subtraction, * Multiplication, / Division)");
-    // TODO: Add error handling here, only allow them to enter a correct operator
-
-    // Generating an answer
-    Console.WriteLine("Answer:");
-    char operatorSelection = OperatorInput();
-    int answer = number1 + operatorSelection + number2;
-    Console.WriteLine(answer);
-
-    Console.WriteLine("What is number 1?");
-    int number1Input = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("What is number 2?");
-    int number2Input = Convert.ToInt32(Console.ReadLine());
-
-    if (number1Input + operatorSelection + number2Input == answer)
+    string number1Input = Console.ReadLine();
+    if (int.TryParse(number1Input, out number1Int))
     {
-        Console.WriteLine("You win!");
-        score++;
+        break;
     }
     else
     {
-        Console.WriteLine("You lose!");
-        Console.WriteLine($"You scored {score}");
-        break;
+        Console.WriteLine("Please enter a valid number");
     }
 }
+
+Console.WriteLine("Enter number 2");
+int number2Int;
+
+while (true)
+{
+    string number2Input = Console.ReadLine();
+    if (int.TryParse(number2Input, out number2Int))
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Please enter a valid number");
+    }
+}
+
 
 // Method used to print instructions to user
 void PrintIntro()
@@ -83,5 +78,3 @@ char OperatorInput()
     }
     return output;
 }
-
-// Determinds if game was won or not
